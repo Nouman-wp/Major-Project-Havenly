6784bf1497fa6e728a10fd2b539bdd96f3a56d39
Nouman <noumanpm@gmail.com>
2025-02-11 13:08:46 +0530

    Data Testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require = require("./models/listing");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/havenly";
 
@@ -13,6 +14,20 @@ main().then(()=>{
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
+});
 
 app.get("/",(req,res)=>{
     res.send("Root Website");
