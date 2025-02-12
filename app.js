8fb426b69f9fea46d09eb8340f973898fe213857
Nouman <noumanpm@gmail.com>
2025-02-12 22:38:26 +0530

    Added Reviews and Comments

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/havenly";
 
@@ -39,6 +40,18 @@ const validateListing = (req,res,next) => {
 };
 
 
+const validateReview = (req,res,next) => {
+  let {error} = reviewSchema.validate(req.body);
+  if (error){
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+  };
+
+
+
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
@@ -58,7 +71,7 @@ app.get("/listings/new", (req, res) => {
 //Show.ejs route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("show.ejs", { listing });
 }));
 
@@ -94,6 +107,34 @@ app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
   res.redirect(`/listings/${id}`);
 })
 );
+
+//Review Route
+// Post
+app.post("/listings/:id/reviews",validateReview,wrapAsync( async (req,res)=>{
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  res.redirect(`/listings/${listing._id}`); 
+}))
+
+// Review Deleting Route
+
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); 
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Root Website");
