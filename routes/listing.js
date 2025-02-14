64a454fb0392afb2f96b803aaad5ea6f83330e94
Nouman <noumanpm@gmail.com>
2025-02-14 20:45:20 +0530

    login.ejs

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
 
 
 
@@ -28,7 +29,7 @@ router.get("/", wrapAsync(async (req, res) => {
   }));
   
   //New Route
-  router.get("/new", (req, res) => {
+  router.get("/new", isLoggedIn,(req, res) => {
     res.render("new.ejs");
   });
   
@@ -44,7 +45,7 @@ router.get("/", wrapAsync(async (req, res) => {
   }));
 
   //edit.ejs route
-  router.get("/:id/edit", wrapAsync(async (req, res) => {
+  router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -57,7 +58,7 @@ router.get("/", wrapAsync(async (req, res) => {
   
   //Create Route
   router.post(
-    "/",
+    "/",isLoggedIn,
     wrapAsync(async (req, res, next) => {
       const newListing = new Listing(req.body.listing);
       await newListing.save();
@@ -67,7 +68,8 @@ router.get("/", wrapAsync(async (req, res) => {
   );
   
   //Delete Route
-  router.delete("/:id", async (req, res) => {
+    "/",
+  router.delete("/:id",isLoggedIn, async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
@@ -76,7 +78,7 @@ router.get("/", wrapAsync(async (req, res) => {
   });
   
   //Update Route
-  router.put("/:id",validateListing, wrapAsync(async (req, res) => {
+  router.put("/:id",isLoggedIn,validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     req.flash("success", "Notification : Listing Updated !");
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 
 //signup
@@ -27,8 +28,18 @@ router.post("/signup", wrapAsync(async(req,res)=>{
 
 // login
 
-router.get("/login",)
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
 
-
+router.post("/login",passport.authenticate("local",{
+    failureRedirect:"/login",
+    failureFlash: true,
+}),
+async(req,res)=>{
+    req.flash("success" , "Welcome Back to Havenly ! You are logged in");
+    res.redirect("/listings");
+}
+);
 
 module.exports = router;
