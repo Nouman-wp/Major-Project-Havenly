3c1ba6aafad7be6f3e1d793a79c2921a9d3282dc
Nouman <noumanpm@gmail.com>
2025-02-14 19:14:14 +0530

    User.js

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
+// Authentication and user password things 
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
   
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -54,6 +58,15 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+//password stuff
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
