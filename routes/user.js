4c2592a417b2d65746f1e2f202837678dad5e55d
Nouman <noumanpm@gmail.com>
2025-02-14 20:00:28 +0530

    signup.ejs

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,34 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+
+
+//signup
+
+router.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync(async(req,res)=>{
+    try{
+        let{username,email,password}=req.body;
+    const newUser =new User({email,username});
+    const registeredUser = await User.register(newUser,password);
+    console.log(registeredUser);
+    req.flash("success","Welcome to Havenly");
+    res.redirect("/listings");
+    }catch(e){
+        req.flash("error" , e.message);
+        res.redirect("/signup");
+    }
+}));
+
+
+// login
+
+router.get("/login",)
+
+
+
+module.exports = router;
