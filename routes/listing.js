10d21b3be3802c6dd3287197cb4a229d7f67a22b
Nouman <noumanpm@gmail.com>
2025-02-15 23:29:59 +0530

    cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -4,11 +4,14 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 
 // index and create route controlled
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+// .post(isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+.post(upload.single('listing[image]'),(req,res)=>{res.send(req.file);})
 
 //New Route controlled
 router.get("/new", isLoggedIn, listingController.renderNewForm);
