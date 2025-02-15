84dc38d7d342960db9df94df11fe8a6ed0f5c786
Nouman <noumanpm@gmail.com>
2025-02-15 19:20:46 +0530

    usersController

--- routes/user.js
+++ routes/user.js
@@ -5,63 +5,22 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
 
+const userController = require("../controllers/users");
+
 //signup
-
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
-
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body; 
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash("success", "Welcome to Havenly");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
-);
+router.get("/signup", userController.renderSignupForm);
+router.post("/signup",wrapAsync(userController.signup));
 
 // login
-
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
-
+router.get("/login", userController.renderLoginForm);
 router.post(
   "/login",saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
-  }),
-  async (req, res) => {
-    req.flash("success", "Welcome Back to Havenly ! You are logged in");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  }
-);
+  }),userController.login);
 
 // logout
-
-router.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "You are now logged out!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
